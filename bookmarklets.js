d67d4ccf87a3f21b613942e7a6f4dda19d324bf7
Simon Legner <Simon.Legner@gmail.com>
2018-08-21 10:28:27 +0200

    Update wappalyzer.com

--- bookmarklets.js
+++ bookmarklets.js
@@ -23,30 +23,31 @@ BOOKMARKLETS.push({
   source: 'https://wappalyzer.com/download',
   fun: function() {
     var d = document,
-    e = d.getElementById('wappalyzer-container');
+      e = d.getElementById('wappalyzer-container');
     if (e !== null) {
       d.body.removeChild(e);
     }
-    var u = 'https://wappalyzer.com/bookmarklet/',
-    t = new Date().getTime(),
-    c = d.createElement('div'),
-    p = d.createElement('div'),
-    l = d.createElement('link'),
-    s = d.createElement('script');
+    var u = 'https://www.wappalyzer.com/',
+      t = new Date().getTime(),
+      c = d.createElement('div'),
+      p = d.createElement('div'),
+      l = d.createElement('link'),
+      s = d.createElement('script');
     c.setAttribute('id', 'wappalyzer-container');
     l.setAttribute('rel', 'stylesheet');
-    l.setAttribute('href', u + 'css/wappalyzer.css');
+    l.setAttribute('href', u + 'css/bookmarklet.css');
     d.head.appendChild(l);
     p.setAttribute('id', 'wappalyzer-pending');
-    p.setAttribute('style', 'background-image: url(' + u + 'images/pending.gif) !important');
+    p.setAttribute('style', 'background-image: url(' + u + 'images/spinner.gif) !important');
     c.appendChild(p);
-    s.setAttribute('src', u + 'js/wappalyzer.js?' + t);
+    s.setAttribute('src', u + 'bookmarklet/wappalyzer.js');
     s.onload = function() {
+      window.wappalyzer = new Wappalyzer();
       s = d.createElement('script');
-      s.setAttribute('src', u + 'js/apps.js?' + t);
+      s.setAttribute('src', u + 'bookmarklet/apps.js');
       s.onload = function() {
         s = d.createElement('script');
-        s.setAttribute('src', u + 'js/driver.js?' + t);
+        s.setAttribute('src', u + 'bookmarklet/driver.js');
         c.appendChild(s);
       };
       c.appendChild(s);
